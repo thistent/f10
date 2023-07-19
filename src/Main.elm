369b84e2a8a88c85099380ea72b96c403d8aaee5
Ken Stanton <this.tent@gmail.com>
2023-07-19 20:20:20 +0200

    Using a Project type to load each card, and Dims is loaded from a Json file over Http.

--- src/Main.elm
+++ src/Main.elm
@@ -11,12 +11,26 @@ import Json.Decode as Jd exposing (Decoder)
 
 
 type alias Model =
-    { text : String
+    { project : Project
     }
 
 
+type alias Project =
+    { title : String
+    , link : String
+    , ada : String
+    , problem : String
+    , solution : String
+    }
+
+
+emptyProject : Project
+emptyProject =
+    Project "Dummy Proj." "" "" "" ""
+
+
 type Msg
-    = GotText (Result Http.Error String)
+    = GotText (Result Http.Error Project)
 
 
 main : Program () Model Msg
@@ -35,13 +49,13 @@ main =
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( { text = "" }
+    ( { project = emptyProject }
     , Http.get
         -- FIXME : This should work both locally and on github!
         -- { url = "/docs/notes/test.md?raw=1"
         --{ url = "https://raw.githubusercontent.com/thistent/f10/main/docs/notes/test.md"
         { url = "notes/test.json"
-        , expect = Http.expectJson GotText testDecoder
+        , expect = Http.expectJson GotText projectDecoder
         }
     )
 
@@ -51,8 +65,8 @@ update msg model =
     case msg of
         GotText res ->
             case res of
-                Ok txt ->
-                    ( { model | text = txt }, Cmd.none )
+                Ok p ->
+                    ( { model | project = p }, Cmd.none )
 
                 Err e ->
                     ( model, Cmd.none )
@@ -92,8 +106,7 @@ view model =
                     [ dims 40.0
                     , El.text " : Distributed Idea Management System"
                     ]
-                , El.paragraph
-                    [ Font.justify ]
+                , El.paragraph [ Font.justify, El.spacing 10 ]
                     [ El.text "This page serves two purposes. It will act as a landing page for all my projects in Cardano Catalyst Fund10, as well as being the first stage (a web-based interface) of my  "
                     , dims 20.0
                     , El.text "  project."
@@ -107,43 +120,17 @@ view model =
                     El.text "Ken Stanton"
                 , el [ Font.bold, Font.size 25 ] <|
                     El.text "My Fund10 Proposals:"
-                , propCard 106578
-                    "Research: Strategically Competing with Mobile Money Markets in Malawi"
-                    "₳15,000"
-                    "Mobile money is commonly used in southeast Africa. Cardano hasn't yet disrupted this market. There are untapped opportunities to improve people's ability to move money as well as Cardano's reach!"
-                    "    I see these transactions all the time. My solution is a local survey & detailed report about how Cardano wallet providers could work to make the switch to a Cardano-based solution as easy as possible."
-                , propCard 105979
-                    "Research: Real DAOs and Optimizing Governance for Parallel Experimentation"
-                    "₳20,000"
-                    "Current DAOs don't really focus much on Autonomy. In a sense, DAOs aren't that Decentralized because they pool funds together and require majority votes to allocate them. Is mob rule even Organized???"
-                    "Are current DAOs are really DAOs? The goal is to produce detailed research and documentation of what it might mean to autonomously organize in a truly decentralized way! Can we maximize experiments???"
-                , propCard 105563
-                    "Research: Unique Pseudonymous Identification of DReps through Joint Content Creation"
-                    "₳20,000"
-                    "Allowing DReps to remain anonymous has value, but there are some dangers to fair governance if very large whales decide to game the system. DReps should be encouraged to consider diverse perspectives."
-                    "                  Research detailing how community discussions can not only help DReps broaden their perspectives, but also be used to validate DReps as unique individuals, making things like quadratic voting possible."
-                , propCard 107701
-                    "Dims: Distributed Idea Management System"
-                    "₳45,000"
-                    "Though collaboration is encouraged, the current ideation process in Catalyst incentivizes siloed competition. How can we make collaboration and convergent solutions the naturally beneficial choice?"
-                    "Dims: The research and development of a tool for building and merging structured graphs of knowledge and code. Highlighting how efforts overlap between different projects and individual interests."
-                , El.paragraph
-                    [ Font.justify
-                    , El.spacing 10
-                    , Font.color <| El.rgb 1.0 0.85 0.65
-                    ]
-                    [ el [ Font.bold ] <| El.text "Note: "
-                    , El.text <| "This will be the landing page for all the progress I've made in my projects! Please come back and see my progress as time goes on! "
-                    , el [ Font.color <| El.rgb 1.0 0.65 0.85 ] <|
-                        El.text model.text
-                    ]
+                , propCard malawi
+                , propCard dao
+                , propCard dreps
+                , propCard model.project
                 ]
             , el [ El.width <| El.fillPortion 1 ] <| El.text ""
             ]
 
 
-propCard : Int -> String -> String -> String -> String -> Element msg
-propCard propNum label money problem solution =
+propCard : Project -> Element Msg
+propCard proj =
     El.column
         [ El.width El.fill
         , El.padding 20
@@ -159,28 +146,31 @@ propCard propNum label money problem solution =
             , Font.size 25
             , Font.bold
             ]
-            { url =
-                "https://cardano.ideascale.com/c/idea/"
-                    ++ String.fromInt propNum
-            , label = El.paragraph [] [ El.text label ]
+            { url = proj.link
+            , label = El.paragraph [] [ El.text proj.title ]
             }
         , el
             [ El.alignRight
             , Font.color <| El.rgb 0.85 1.0 0.65
             ]
           <|
-            El.text money
+            El.text proj.ada
         , El.column [ El.spacing 10 ]
             [ el [ Font.bold, Font.size 25 ] <| El.text "Problem:"
-            , El.paragraph [ Font.justify, El.spacing 10 ]
-                [ El.text problem ]
+            , contentBlock
+                [ El.text proj.problem ]
             ]
         , El.column [ El.spacing 10 ]
             [ el [ Font.bold, Font.size 25 ] <| El.text "Solution:"
-            , El.paragraph [ Font.justify, El.spacing 10 ]
-                [ El.text solution ]
+            , contentBlock
+                [ El.text proj.solution ]
             ]
         ]
+
+
+contentBlock : List (Element Msg) -> Element Msg
+contentBlock =
+    El.paragraph [ Font.justify, El.spacing 10 ]
 
 
 dims : Float -> Element Msg
@@ -194,9 +184,14 @@ dims fs =
         }
 
 
-testDecoder : Decoder String
-testDecoder =
-    Jd.field "problem" Jd.string
+projectDecoder : Decoder Project
+projectDecoder =
+    Jd.map5 Project
+        (Jd.field "title" Jd.string)
+        (Jd.field "link" Jd.string)
+        (Jd.field "ada" Jd.string)
+        (Jd.field "problem" Jd.string)
+        (Jd.field "solution" Jd.string)
 
 
 edges =
@@ -204,4 +199,38 @@ edges =
     , bottom = 0
     , right = 0
     , left = 0
+    }
+
+
+
+-- Projects --
+
+
+malawi : Project
+malawi =
+    { title = "Research: Strategically Competing with Mobile Money Markets in Malawi"
+    , link = "https://cardano.ideascale.com/c/idea/106578"
+    , ada = "₳15,000"
+    , problem = "Mobile money is commonly used in southeast Africa. Cardano hasn't yet disrupted this market. There are untapped opportunities to improve people's ability to move money as well as Cardano's reach!"
+    , solution = "    I see these transactions all the time. My solution is a local survey & detailed report about how Cardano wallet providers could work to make the switch to a Cardano-based solution as easy as possible."
+    }
+
+
+dao : Project
+dao =
+    { title = "Research: Real DAOs and Optimizing Governance for Parallel Experimentation"
+    , link = "https://cardano.ideascale.com/c/idea/105979"
+    , ada = "₳20,000"
+    , problem = "Current DAOs don't really focus much on Autonomy. In a sense, DAOs aren't that Decentralized because they pool funds together and require majority votes to allocate them. Is mob rule even Organized???"
+    , solution = "Are current DAOs are really DAOs? The goal is to produce detailed research and documentation of what it might mean to autonomously organize in a truly decentralized way! Can we maximize experiments???"
+    }
+
+
+dreps : Project
+dreps =
+    { title = "Research: Unique Pseudonymous Identification of DReps through Joint Content Creation"
+    , link = "https://cardano.ideascale.com/c/idea/105563"
+    , ada = "₳20,000"
+    , problem = "Allowing DReps to remain anonymous has value, but there are some dangers to fair governance if very large whales decide to game the system. DReps should be encouraged to consider diverse perspectives."
+    , solution = "                  Research detailing how community discussions can not only help DReps broaden their perspectives, but also be used to validate DReps as unique individuals, making things like quadratic voting possible."
     }
